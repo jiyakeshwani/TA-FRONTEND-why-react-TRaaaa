88dbf1016f60458a52f3813aedaecb3523e52d20
jiyakeshwani <jiyakeshwani4@gmail.com>
2022-06-19 00:18:48 +0530

    block5 why react

---
+++ block-BRaaae/code/script.js
@@ -0,0 +1,80 @@
+let input = document.querySelector(`input[type = "text"]`);
+let rootElm = document.querySelector(".movie-list");
+
+let allMovies = [
+  {
+    name: "Incidious",
+    watched: false,
+  },
+  {
+    name: "Forest gump",
+    watched: false,
+  },
+];
+
+function handleClick(event) {
+  let id = event.target.id;
+  allMovies[id].watched = !allMovies[id].watched;
+}
+
+function deleteMovie(event) {
+  let id = event.target.id;
+  allMovies.splice(id, 1);
+  createUI();
+}
+
+// function createElement(type, attr = {}, ...children) {
+//   let element = document.createElement(type);
+//   for (let key in attr) {
+//     if (key.startsWith("data-")) {
+//       element.setAttribute(key, attr[key]);
+//     } else if (key.startsWith("on")) {
+//       let eventType = key.replace("on", "").toLowerCase();
+//       element.addEventListener(eventType, attr[key]);
+//     } else {
+//       element[key] = attr[key];
+//     }
+//   }
+//   children.forEach((child) => {
+//     if (typeof child === "object") {
+//       element.append(child);
+//     }
+//     if (typeof child === "string") {
+//       let node = document.createTextNode(child);
+//       element.append(node);
+//     }
+//   });
+//   return element;
+// }
+
+let createElement = React.createElement;
+
+function createUI() {
+  let ui = allMovies.map((movie) => {
+    let li = createElement(
+      "li",
+      {},
+      createElement("h2", {}, movie.name),
+      createElement("input", {
+        type: "checkbox",
+        checked: movie.watched,
+        onClick: handleClick,
+      }),
+      createElement("span", { onClick: deleteMovie }, "Delete")
+    );
+
+    return li;
+  });
+  ReactDOM.render(ui, rootElm);
+}
+
+input.addEventListener("keyup", (event) => {
+  if (event.keyCode === 13) {
+    allMovies.push({
+      name: event.target.value,
+      watched: false,
+    });
+    event.target.value = "";
+    createUI();
+  }
+});
