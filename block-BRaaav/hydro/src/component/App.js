cb6a0841659c4828f468b166c1faf1cf2da8e7c1
jiyakeshwani <jiyakeshwani4@gmail.com>
2022-06-24 23:21:22 +0530

    block2 assignmentlevel 2 3

---
+++ block-BRaaav/hydro/src/component/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import About from "./About";
+import Footer from "./Footer";
+import Header from "./Header";
+import Hero from "./Hero";
+import Blog from "./Blog";
+import Work from "./Work";
+import Contact from "./Contact";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <About />
+      <Blog />
+      <Work />
+      <Contact />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
